2a393899259b8305fb59bdd7ab1ab5f8e9a715f5
Al Shariar <35493040+Shariarbup@users.noreply.github.com>
2024-06-16 12:14:52 +0600

    added validation for text field

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/model/user';
 
 @Component({
   selector: 'app-signup',
@@ -6,9 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-  topics = ['Java', 'Angular', 'Full Stack']
+  topics = ['Java', 'Angular', 'Full Stack'];
+  userModel : User = new User("Al Shariar", "itmasjoy@gmail.com", 1968385155, new Date(), "123", "123", "Java" , "morning", false) ;
+  
   signUp(data: Object): void {
       console.log("🚀 ~ SignupComponent ~ signUp ~ data:", data)
       
   }
+
+  // The control has been visited - ng-touched ng-untouched
+  // The control has been visted - ng-dirty ng-pristine
+  // The controls value is ng-valid ng-invalid
+
+  // ngModel Properties:
+  // class - properties
+  // ng-untouched - untouched
+  // ng-touched - touched
+  // ng-pristine - pristine
+  // ng-dirty - dirty
+  // ng-valid - valid
+  // ng-invalid - invalid
 }
--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,2 +1,15 @@
 export class User {
+    constructor(
+        public name: string,
+        public email: string,
+        public phone: number,
+        public birthDate: Date,
+        public password: string,
+        public confirmPassword: string,
+        public topic: string,
+        public timePreferance: string,
+        public subscribe: boolean,
+
+
+    ) {}
 }
